b833e93d0ce81ea1799988e023324f2e1191fd25
nicolas <contrerasr.nicolas@gmail.com>
2023-05-28 17:38:42 -0300

    QuintaVersion

--- src/Componentes/Menu.js
+++ src/Componentes/Menu.js
@@ -1,5 +1,4 @@
 import React from "react";
-
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
 
@@ -9,11 +8,18 @@ const Menu = () => {
       <Container>
         <Navbar expand="lg" variant="dark" bg="dark">
           <Container>
+
             <Navbar.Brand href="#">Inicio</Navbar.Brand>
-            <Navbar.Brand href="#">Editar</Navbar.Brand>
+
+            <Navbar.Brand 
+            href="#">Editar</Navbar.Brand>
+
             <Navbar.Brand href="#">Ver</Navbar.Brand>
-            <Navbar.Brand href="#">Ayuda</Navbar.Brand>
+
+            <Navbar.Brand href="https://compragamer.com/" target="_Blank">Tienda</Navbar.Brand>
+
             <Navbar.Brand href="#">Acerda De</Navbar.Brand>
+
           </Container>
         </Navbar>
       </Container>
